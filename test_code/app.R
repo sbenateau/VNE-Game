2180e5575cfea9c3a926976ea8c4f9d9999dd8f0
sbenateau <sbenateau@mailodie.fr>
2021-01-27 16:26:44 +0100

    Youhou

--- test_code/app.R
+++ test_code/app.R
@@ -97,8 +97,10 @@ server <- function(input, output, session) {
     
     observeEvent(data_values, {
         for (i in c("oiseaux", "escargots", "sauvages", "vdt")){
-            URL_data_VNE <- RCurl::getURL(paste0("https://depot.vigienature-ecole.fr/datasets/test/papers/", i, ".csv"), .encoding = "UTF-8")
-            data_values[[i]] <- read.csv(text = URL_data_VNE, encoding = 'UTF-8')
+            #URL_data_VNE <- RCurl::getURL(paste0("https://depot.vigienature-ecole.fr/datasets/test/papers/", i, ".csv"), .encoding = "UTF-8")
+        data_values[[i]] <- read.csv(paste0("../../../github/Requetes-et-restitutions/R-pour-restitutions/import_add_data/papers/",i,".csv")
+                #text = URL_data_VNE
+                , encoding = 'UTF-8')
         }
     })
     
