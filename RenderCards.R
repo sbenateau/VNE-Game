35b4fddb6ccb3024ca874ea02e69fdbdb51abdae
sbenateau <sbenateau@mailodie.fr>
2021-02-04 23:12:37 +0100

    remove old code

--- RenderCards.R
+++ RenderCards.R
@@ -13,7 +13,7 @@ renderCardsUI <- function(id, parsedCode) {
   # retrieve first letter: what does the tool do
   # DISABLED: keep toolCode if needed furtherly
   toolID = unlist(strsplit(toolCode, ""))[1]
-  
+  print(toolID)
   # return UI
   return(
     bsCollapsePanel(
@@ -54,10 +54,6 @@ renderCards <- function(input, output, session,
                         id, parsedCode, Results, fullCode) {
   toolCode <- parsedCode[id]
   
-  # retrieve first letter: what does the tool do
-  # DISABLED: keep toolCode if needed furtherly
-  # toolCode = unlist(strsplit(toolCode, ""))[1]
-  
   # values are returned in `rv` in app.R
   output[[id]] <- switch(unlist(strsplit(toolCode, ""))[1],
                          G = renderPlot(Results),
@@ -69,52 +65,6 @@ renderCards <- function(input, output, session,
                          N = DT::renderDataTable(Results, options = list(pageLength = 24, dom = 'tp', searching = FALSE), escape = FALSE),
                          E = DT::renderDataTable(Results, options = list(pageLength = 24, dom = 'tp', searching = FALSE), escape = FALSE),
                          V = DT::renderDataTable(Results, options = list(pageLength = 24, dom = 'tp', searching = FALSE), escape = FALSE),
-                         S = renderUI({
-                           # Découpage du code
-                           informations <- codeInformation(fullCode)
-                           
-                           if (!"D" %in% informations$toolUsed) { # Pas de données
-                             # Il manque les données : vidéo à refaire
-                             link = "1143628"
-                           } else if (length(informations$toolUsed) == 1){ # Seulement les données
-                             link = "377267769"
-                           } else if ("M" %in% informations$toolUsed){ # utilisation de random all (non souhaité)
-                             # améliorer la vidéo en n'ayant pas forcement de graph
-                             link = "377267700"
-                           } else if ("P" %in% informations$toolUsed) { # Utilisation d'une carte (non souhaité)
-                             # Pas de carte nécessaire pour répondre à la question : vidéo à refaire
-                             link = "1143628"
-                           } else if (!"Env" %in% informations$varUsed){ # Pas env
-                             # video à faire manque la carte environnement
-                             link = "1143628"
-                           } else if (!"R" %in% informations$toolUsed){ # Pas de regroupement
-                             # regroupement souhaitable
-                             link = "377267820"
-                           } else if ("So" %in% informations$funUsed & !"Mo" %in% informations$funUsed){ # Seulement une somme
-                             # fonction non adaptée
-                             link = "377267625"
-                           } else if ("Co" %in% informations$funUsed){ # comptage
-                             # fonction non adaptée
-                             link = "377267747"
-                           } else if (!"Pla" %in% informations$varUsed){ # Pas placette
-                             # video à faire manque la carte placette
-                             link = "1143628"
-                           } else if (!"Num" %in% informations$varUsed){ # Pas numéro d'observation
-                             # video à faire manque la carte placette
-                             link = "1143628"
-                           } else if (!"G" %in% informations$toolUsed){ # No graph
-                             # Manque de graph nécessaire : vidéo à refaire
-                             link = "1143628"
-                           } else if (!"B" %in% informations$toolUsed){ # No error bars
-                             link = "1143628"
-                           } else if ("TOUT BONNN"){ # good graph
-                             link = "377267723"
-                           } else { # nope
-                             link = sample(c("377267872", "377267851", "377267555"))
-                             
-                           }
-                           HTML(paste0('<iframe src="https://player.vimeo.com/video/',link,'" width="640" height="480" frameborder="0" allow="autoplay" allowfullscreen></iframe>'))
-                         }),
                          renderTable(head(Results,40)))
   
 }
